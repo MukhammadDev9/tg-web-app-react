2810a67287e6a334f4714adf0bdb797fcbf94889
Mukhammad <mukhammaddev9@gmail.com>
2023-06-09 19:26:25 +0500

    init

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import "./Button.css";
 
 const Button = (props) => {
     return <button {...props} className={clsx("button", props.className)} />;
